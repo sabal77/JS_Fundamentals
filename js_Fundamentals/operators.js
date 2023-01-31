021a963ce4912052f25823d31597c99df80c961b
PYIE <sabalhumagain123@gmail.com>
2023-02-01 00:19:39 +0545

    Basic Operators

---
+++ js_Fundamentals/operators.js
@@ -0,0 +1,13 @@
+let x = 5;
+let y = 7.1;
+
+
+console.log("addition is: ", (x+y))
+console.log("subtraction is: ", (x-y))
+console.log("Multiplication is: ", (x*y))
+console.log("modulus is: ", (x%y))
+console.log("division is: ", `${(x/y).toFixed(3)}`)
+console.log("increment is: ", (x++))
+console.log("decrement is: ", (y--))
+
+
