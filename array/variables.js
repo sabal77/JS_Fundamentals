2786ae9f13e556be68e47b08cbde9286f25d8b9d
PYIE <sabalhumagain123@gmail.com>
2023-02-01 00:00:49 +0545

    introduction to js

---
+++ array/variables.js
@@ -0,0 +1,3 @@
+//variables are initialized by var 
+//let allows yoy to declare variables that are limited to the scope of a block
+//var declares variables globally
---
+++ js_Fundamentals/introduction.js
@@ -0,0 +1,17 @@
+//variables are initialized by var 
+//let allows yoy to declare variables that are limited to the scope of a block
+//var declares variables globally
+//const is used for constant variabels i.e. whoose value cannot be changed
+//tempelate literals are eclosed by backticks ``
+//commonly used for string interpolation
+//cannot declare variables whose values cannot be changed.
+//console.log is  to print out to console and is used to write message
+//js object, js type, js symbol, typeof
+let number1= 5;
+var number2= 3;
+console.log(typeof(number1));
+console.log("The number is: " + `${(number2 + number1)}`);
+
+//prompting variable.
+//const name =prompt("Enter name: ");
+//console.log("hello", name);
